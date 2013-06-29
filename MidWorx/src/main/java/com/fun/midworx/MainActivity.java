ad0881948943ec4cf90b1a1178cf869039b3b372
Rotemts <rotem.ts0@gmail.com>
2013-06-29 19:55:40 +0300

    Created WordsBox

--- MidWorx/src/main/java/com/fun/midworx/MainActivity.java
+++ MidWorx/src/main/java/com/fun/midworx/MainActivity.java
@@ -3,13 +3,50 @@ package com.fun.midworx;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+import com.fun.midworx.com.fun.midworx.views.WordsBox;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends Activity {
+    ArrayList<WordsBox> mBoxes = new ArrayList<WordsBox>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //dummy data
+        ArrayList<String> words3 = new ArrayList<String>(Arrays.asList("rtg","nsr","uda","sdf","yui"));
+        ArrayList<String> words4 = new ArrayList<String>(Arrays.asList("rtdg","nsar","yisr"));
+        ArrayList<String> words5 = new ArrayList<String>(Arrays.asList("rtgsr","udsra"));
+        ArrayList<String> words6 = new ArrayList<String>(Arrays.asList("rtguda"));
+
+
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,LinearLayout.LayoutParams.WRAP_CONTENT);
+        params.setMargins(5,5,5,5);
+        LinearLayout boxesLayout= (LinearLayout) findViewById(R.id.words_boxes_layout);
+
+        addBox(boxesLayout,params,words3);
+        addBox(boxesLayout,params,words4);
+        addBox(boxesLayout,params,words5);
+        addBox(boxesLayout,params,words6);
     }
-    
+
+    private void addBox(LinearLayout boxesLayout,LinearLayout.LayoutParams params, ArrayList<String> words) {
+        WordsBox box = new WordsBox(this,words);
+        boxesLayout.addView(box,params);
+        mBoxes.add(box);
+    }
+
+    private void guessWord(String word) {
+
+
+    }
+
 }
---
+++ MidWorx/src/main/java/com/fun/midworx/com/fun/midworx/views/WordsBox.java
@@ -0,0 +1,79 @@
+package com.fun.midworx.com.fun.midworx.views;
+
+import android.content.Context;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * Created by Rotem on 29/06/13.
+ */
+public class WordsBox extends LinearLayout {
+
+    private final int mLettersNum;
+    private final HashMap<String,WordView> mWords = new HashMap<String, WordView>();
+    private final Context c;
+    private String mEmpty;
+    private int mGuessed = 0;
+
+    public WordsBox(Context context, ArrayList<String> words) {
+        super(context);
+        setOrientation(VERTICAL);
+        setPadding(5,5,5,5);
+
+        mLettersNum = words.size() > 0 ? words.get(0).length() : 0;
+        setBackgroundColor(0xffff0000 * mLettersNum / 6);
+
+        c = context;
+
+        //create empty text before word is guessed
+        mEmpty = "";
+        for (int i=0 ; i<mLettersNum ; i++)
+            mEmpty += "_ ";
+
+        //create a TextView for each word
+        for (int i=0 ; i<words.size() ; i++)
+            mWords.put(words.get(i), addWordView());
+    }
+
+    public void guessWord(String word) {
+        if (mWords.containsKey(word)) {
+            WordView wordView = mWords.get(word);
+            //only handle the guess if this is the first time the user guesses this word
+            if (!wordView.isGuessed()) {
+                wordView.setGuessed(true);
+                wordView.setText(word);
+                mGuessed++;
+                //check if we guessed all words
+                if (mGuessed == mWords.size())
+                    setBackgroundColor(0xff00ff00);
+            }
+        }
+    }
+
+    private WordView addWordView() {
+        WordView wordView = new WordView(c);
+        wordView.setText(mEmpty);
+        addView(wordView);
+        return wordView;
+    }
+
+    private class WordView extends TextView {
+        private boolean mGuessed = false;
+
+        public WordView(Context context) {
+            super(context);
+        }
+
+        private boolean isGuessed() {
+            return mGuessed;
+        }
+
+        private void setGuessed(boolean guessed) {
+            this.mGuessed = guessed;
+        }
+    }
+}
