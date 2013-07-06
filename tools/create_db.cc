9a8827037a62fedcb0ef477d98c045dab342d0e5
misha <mishas@mishas-macbookpro.local>
2013-07-06 15:56:53 +0300

    Added index creator script, and base index for words.

---
+++ tools/create_db.cc
@@ -0,0 +1,89 @@
+#include <map>
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <set>
+#include <vector>
+
+using namespace std;
+
+void rtrim(string& s) {
+  s.erase(std::find_if(s.rbegin(), s.rend(), std::not1(std::ptr_fun<int, int>(std::isspace))).base(), s.end());
+}
+
+map<char, size_t> break_word(string word) {
+  map<char, size_t> broken;
+  for (size_t i = 0; i < word.size(); ++i) {
+    broken[word[i]]++;
+  }
+  return broken;
+}
+
+pair<map<map<char, size_t>, uint32_t>, set<map<char, size_t> > > read_words(string fname) {
+  map<map<char, size_t>, uint32_t> all_words;
+  set<map<char, size_t> > six_letter_words;
+  ifstream file(fname.c_str());
+  string word;
+  uint32_t seek;
+  while (!file.eof()) {
+    seek = file.tellg();
+    getline(file, word);
+    rtrim(word);
+    std::transform(word.begin(), word.end(), word.begin(), ::tolower);
+    map<char, size_t> broken_word = break_word(word);
+    if ((word.size() < 3) || (word.size() > 6)) {
+      continue;
+    }
+    all_words[broken_word] = seek;
+    if (word.size() == 6) {
+      sort(word.begin(), word.end());
+      six_letter_words.insert(broken_word);
+    }
+  }
+  cout << all_words.size() << endl;
+  file.close();
+  return make_pair(all_words, six_letter_words);
+}
+
+bool check_word_in_word(map<char, size_t> short_word, map<char, size_t> long_word) {
+  for (map<char, size_t>::const_iterator short_iter = short_word.begin(); short_iter != short_word.end(); ++short_iter) {
+    if (short_iter->second > long_word[short_iter->first]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+void create_index(string fname, const set<map<char, size_t> >& six_letter_words, const map<map<char, size_t>, uint32_t>& all_words) {
+  ofstream file(fname.c_str());
+  uint32_t num_six_letter_words = six_letter_words.size();
+  file.write(reinterpret_cast<char*>(&num_six_letter_words), sizeof(uint32_t));
+
+  vector<pair<uint32_t, uint32_t> > index_index;
+  vector<uint32_t> word_index;
+
+  uint32_t i = 0;
+  for (set<map<char, size_t> >::const_iterator iter = six_letter_words.begin(); iter != six_letter_words.end(); ++iter, ++i) {
+    uint32_t word_count = 0;
+    for (map<map<char, size_t>, uint32_t>::const_iterator words_iter = all_words.begin(); words_iter != all_words.end(); ++words_iter) {
+      if (check_word_in_word(words_iter->first, *iter)) {
+        word_count++;
+        word_index.push_back(words_iter->second);
+      }
+    }
+    index_index.push_back(make_pair(i, word_count));
+  }
+
+  file.write(reinterpret_cast<char*>(index_index.data()), index_index.size() * sizeof(uint32_t) * 2);
+  file.write(reinterpret_cast<char*>(word_index.data()), word_index.size() * sizeof(uint32_t));
+
+  file.close();
+}
+
+int main() {
+  pair<map<map<char, size_t>, uint32_t>, set<map<char, size_t> > > pair_words = read_words("./words");
+  map<map<char, size_t>, uint32_t> all_words = pair_words.first;
+  set<map<char, size_t> > six_letter_words = pair_words.second;
+  create_index("./db", six_letter_words, all_words);
+  return 0;
+}
