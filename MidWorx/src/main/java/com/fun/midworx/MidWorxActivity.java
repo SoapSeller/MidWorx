175c82d0707bd3690f752dcfee9e66ae4cd67cf9
misha <mishas@mishas-macbookpro.local>
2013-07-06 19:44:20 +0300

    Hiding system ui in all places.

--- MidWorx/src/main/java/com/fun/midworx/MidWorxActivity.java
+++ MidWorx/src/main/java/com/fun/midworx/MidWorxActivity.java
@@ -3,20 +3,33 @@ package com.fun.midworx;
 import android.annotation.TargetApi;
 import android.app.Activity;
 import android.os.Build;
+import android.os.Bundle;
 import android.view.View;
 
 public class MidWorxActivity extends Activity {
     @Override
-    protected void onResume() {
-        super.onResume();
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
 
         if (Build.VERSION.SDK_INT >= 11) {
             dimSystemUi();
         }
     }
 
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+
+        if (hasFocus) {
+            if (Build.VERSION.SDK_INT >= 11) {
+                dimSystemUi();
+            }
+        }
+    }
+
     @TargetApi(11)
     private void dimSystemUi() {
-        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE);
+        final View view = getWindow().getDecorView();
+        view.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE);
     }
 }
