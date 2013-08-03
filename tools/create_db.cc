88f73e14194d41e19453fc1a7ed3993521f7f6b0
romansky <roman.land@gmail.com>
2013-08-03 19:37:20 +0300

    +fix missing words due to map bug in create_db.cc; +improve word debug tool

--- tools/create_db.cc
+++ tools/create_db.cc
@@ -4,6 +4,7 @@
 #include <set>
 #include <string>
 #include <vector>
+#include <algorithm>
 
 /* This program generates the index file for words.
  * Format of the index file:
@@ -17,6 +18,8 @@
  */
 
 using namespace std;
+
+typedef map<pair<string, map<char, size_t> >, uint32_t> AllWords;
 
 void rtrim(string& s) {
   s.erase(std::find_if(s.rbegin(), s.rend(), std::not1(std::ptr_fun<int, int>(std::isspace))).base(), s.end());
@@ -34,8 +37,8 @@ bool not_alpha(char c) {
   return !isalpha(c);
 }
 
-pair<map<map<char, size_t>, uint32_t>, set<map<char, size_t> > > read_words(string fname) {
-  map<map<char, size_t>, uint32_t> all_words;
+pair<AllWords, set<map<char, size_t> > > read_words(string fname) {
+  AllWords all_words;
   set<map<char, size_t> > six_letter_words;
   ifstream file(fname.c_str());
   string word;
@@ -50,7 +53,7 @@ pair<map<map<char, size_t>, uint32_t>, set<map<char, size_t> > > read_words(stri
         (find_if(word.begin(), word.end(), not_alpha) != word.end())) {
       continue;
     }
-    all_words[broken_word] = seek;
+    all_words[make_pair(word, broken_word)] = seek;
     if (word.size() == 6) {
       sort(word.begin(), word.end());
       six_letter_words.insert(broken_word);
@@ -69,7 +72,7 @@ bool check_word_in_word(map<char, size_t> short_word, map<char, size_t> long_wor
   return true;
 }
 
-void create_index(string fname, const set<map<char, size_t> >& six_letter_words, const map<map<char, size_t>, uint32_t>& all_words) {
+void create_index(string fname, const set<map<char, size_t> >& six_letter_words, const AllWords& all_words) {
   ofstream file(fname.c_str());
   uint32_t num_six_letter_words = six_letter_words.size();
   file.write(reinterpret_cast<char*>(&num_six_letter_words), sizeof(uint32_t));
@@ -80,8 +83,8 @@ void create_index(string fname, const set<map<char, size_t> >& six_letter_words,
   uint32_t idx = 0;
   for (set<map<char, size_t> >::const_iterator iter = six_letter_words.begin(); iter != six_letter_words.end(); ++iter) {
     uint32_t word_count = 0;
-    for (map<map<char, size_t>, uint32_t>::const_iterator words_iter = all_words.begin(); words_iter != all_words.end(); ++words_iter) {
-      if (check_word_in_word(words_iter->first, *iter)) {
+    for (AllWords::const_iterator words_iter = all_words.begin(); words_iter != all_words.end(); ++words_iter) {
+      if (check_word_in_word(words_iter->first.second, *iter)) {
         word_count++;
         word_index.push_back(words_iter->second);
       }
@@ -101,9 +104,9 @@ int main(int argc, char** argv) {
     cout << "Usage: " << argv[0] << " input_file output_file" << endl;
     return 1;
   }
-  pair<map<map<char, size_t>, uint32_t>, set<map<char, size_t> > > pair_words = read_words(argv[1]);
-  map<map<char, size_t>, uint32_t> all_words = pair_words.first;
-  set<map<char, size_t> > six_letter_words = pair_words.second;
+  pair<AllWords, set<map<char, size_t> > > pair_words = read_words(argv[1]);
+  AllWords& all_words = pair_words.first;
+  set<map<char, size_t> >& six_letter_words = pair_words.second;
   create_index(argv[2], six_letter_words, all_words);
   return 0;
 }
--- tools/debug_file.py
+++ tools/debug_file.py
@@ -4,6 +4,10 @@ def to_num(x):
 six_to_sub_words = {}
 offset_to_numwords = {}
 six_letter_word_count = None
+
+bytes_to_word = {}
+word_to_byte = {}
+
 english_words = open("../MidWorx/src/main/assets/words").read().split("\r\n")
 
 def bytes_from_file(filename, chunksize=8192):
@@ -14,33 +18,81 @@ def bytes_from_file(filename, chunksize=8192):
 		yield chunk
             else:
                 break
-def find_word(word):
-	wordIndex = english_words.index(word)
-	
+
+
+currentByteKey = None
+byteCount = 0
+
+bytes_to_word[0] = []
+for b in bytes_from_file("../MidWorx/src/main/assets/words",1):
+	if (b == "\r"):
+		if currentByteKey != None:
+			word_to_byte[ "".join(bytes_to_word[currentByteKey]) ] = currentByteKey
+		currentByteKey = None
+	else:
+		if (b != "\r" and b != "\n"):
+			if currentByteKey == None:
+				currentByteKey = byteCount
+				bytes_to_word[currentByteKey] = []
+			bytes_to_word[currentByteKey].append(b)
+
+	byteCount += 1
+
 
 c = 0
+vc = 0
+current_vc = 0
+current_key = None
 key = None
 tmp_buffer = []
-for b in bytes_from_file("../MidWorx/src/main/assets/words.idx",4):
+#for b in bytes_from_file("../MidWorx/src/main/assets/words.idx",4):
+for b in bytes_from_file("bla.idx",4):
 	num_val = to_num(b)
 	if not six_letter_word_count:
 		six_letter_word_count = num_val
 	else:
+		# initial mapping of word sectinos
 		if c < six_letter_word_count:
 			if key is None:
 				key = num_val
 			else:
-				six_to_sub_words[key] = num_val
+				offset_to_numwords[key] = num_val
 				key = None
 				c += 1
+		# actual word mapping action
 		else:
-			loc_index = c - six_letter_word_count
-			if six_to_sub_words.has_key(loc_index):
-				if len(tmp_buffer):
-					
-				
-				
-			offset_to_numwords[c - six_letter_word_count] = num_val
-			c += 2
-	
-print find_word("yuppie")
+			if offset_to_numwords.has_key(vc):
+				current_key = num_val
+				if vc != 0 and current_vc + offset_to_numwords[current_vc] == vc:
+					for widx in tmp_buffer:
+						if len(bytes_to_word[widx]) == 6:
+							six_to_sub_words[widx] = tmp_buffer
+				tmp_buffer = []
+				current_vc = vc
+			tmp_buffer.append(num_val)
+			vc += 1 
+
+
+
+# def find_word(word):
+	# wordIndex ]
+
+bad_word = "fibred"
+# 52528
+# loca = word_to_byte["abandon"]
+# print bytes_to_word[loca]
+
+# print sorted(bytes_to_word.keys())
+# for num in sorted(six_to_sub_words.keys()):
+# 	print bytes_to_word[num]
+# print six_to_sub_words
+# print bytes_to_word[2466]
+bad_loc = word_to_byte[bad_word]
+# print bad_loc
+# print bytes_to_word
+for num in six_to_sub_words[bad_loc]:
+	print bytes_to_word[num]
+# 0, 8, 19, 31, 41, 45, 52, 59, 67, 77, 84, 93, 103, 114, 123, 134, 145
+# print max(six_to_sub_words.keys())
+# print find_word("key")
+# print six_to_sub_words
